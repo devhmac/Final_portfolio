20852b49e3768f4b24ba15c504e2661a66111179
Devin MacGillivray <devhmac@gmail.com>
2022-06-22 07:03:27 -0600

    sanity schema

--- sanity_backend/schemas/testimonials.js
+++ sanity_backend/schemas/testimonials.js
@@ -3,6 +3,21 @@ export default {
   title: 'Testimonials',
   type: 'document',
   fields: [{
-    name: 'name'
+    name: 'name',
+    title: 'Name',
+    type: 'string',
+  }, {
+    name: 'company',
+    title: 'Company',
+    type: 'string',
+  }, {
+    name: 'imageurl',
+    title: 'ImgURL',
+    type: 'image',
+    options: { hotspot: true, }
+  }, {
+    name: 'feedback',
+    title: 'Feedback',
+    type: 'string',
   }],
 }
