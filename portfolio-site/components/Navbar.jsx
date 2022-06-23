9f3dbb13d4d930dfd3a79373867c2745fcc713a7
Devin MacGillivray <devhmac@gmail.com>
2022-06-23 06:58:44 -0600

    styling nav

--- portfolio-site/components/Navbar.jsx
+++ portfolio-site/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import styles from "../styles/Navbar.module.scss";
 import homeStyles from "../styles/home.module.scss";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className={styles.app__navbar}>
-      <div className={styles["app__navbar-logo"]}>Devin MacGillivray</div>
-      <ul>
+      <div className={styles["app__navbar-logo"]}>
+        <Link href="/">Devin MacGillivray</Link>
+      </div>
+      <ul className={styles["app__navbar-links"]}>
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li
             className={`${homeStyles["app__flex"]} ${homeStyles["p-text"]}`}
