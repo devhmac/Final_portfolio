b7863d5825dca7ccd8416a7d973ce82b14cb006d
Devin MacGillivray <devhmac@gmail.com>
2022-06-22 19:14:46 -0600

    importing scss

---
+++ portfolio-site/container/Testimonial.jsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Testimonial.module.scss";
+
+const Testimonial = () => {
+  return <div>Testimonial</div>;
+};
+
+export default Testimonial;
---
+++ portfolio-site/container/Work.jsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Work.module.scss";
+
+const Work = () => {
+  return <div></div>;
+};
+
+export default Work;
