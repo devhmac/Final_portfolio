0527ca4fc76be12b7da25ae9dd5ce8db0854d008
Devin MacGillivray <devhmac@gmail.com>
2022-06-22 20:24:58 -0600

    maping list keys for links later

--- portfolio-site/components/Navbar.jsx
+++ portfolio-site/components/Navbar.jsx
@@ -2,8 +2,16 @@ import styles from "../styles/Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.app__navbar}>
       <div>Devin MacGillivray</div>
+      <ul>
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
