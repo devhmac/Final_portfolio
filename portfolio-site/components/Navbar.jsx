c985de350bd65cc997d491c94f5fd0bed5e55cb3
Devin MacGillivray <devhmac@gmail.com>
2022-06-23 06:36:39 -0600

    styling nav and testing nextjs

--- portfolio-site/components/Navbar.jsx
+++ portfolio-site/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Navbar.module.scss";
+import homeStyles from "../styles/home.module.scss";
 
 const Navbar = () => {
   return (
@@ -6,7 +7,10 @@ const Navbar = () => {
       <div className={styles["app__navbar-logo"]}>Devin MacGillivray</div>
       <ul>
         {["home", "about", "work", "skills", "contact"].map((item) => (
-          <li key={`link-${item}`}>
+          <li
+            className={`${homeStyles["app__flex"]} ${homeStyles["p-text"]}`}
+            key={`link-${item}`}
+          >
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
