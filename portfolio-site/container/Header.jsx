a30cb375c11e3db5b9ca82547f30309476cf4fc8
Devin MacGillivray <devhmac@gmail.com>
2022-06-24 06:35:50 -0600

    brings in photo, start on header

--- portfolio-site/container/Header.jsx
+++ portfolio-site/container/Header.jsx
@@ -1,7 +1,20 @@
 import styles from "../styles/Header.module.scss";
+import { motion } from "framer-motion";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={`${styles["app__header"]} ${styles["app__flex"]}`}>
+      Header
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className={styles["app_header-info"]}
+      ></motion.div>
+      <div className={styles["app__header-badge"]}>
+        <div className={`${styles["badge-cmp"]} ${styles.app__flex}`}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
