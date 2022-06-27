8708759ce27d13ad7027d9493d7eaeac360db37b
Devin MacGillivray <devhmac@gmail.com>
2022-06-27 06:53:23 -0600

    adding skills for about

--- portfolio-site/container/About.jsx
+++ portfolio-site/container/About.jsx
@@ -1,7 +1,54 @@
 import styles from "../styles/About.module.scss";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const abouts = [
+  {
+    title: "Customer Relationships",
+    description: "I am a dev dev dev",
+    imgURL: "",
+  },
+  {
+    title: "Service Innovation",
+    description: "I am a dev dev dev",
+    imgURL: "",
+  },
+  {
+    title: "Business Operations",
+    description: "I am a dev dev dev",
+    imgURL: "",
+  },
+  {
+    title: "Full Stack Development",
+    description: "I am a dev dev dev",
+    imgURL: "",
+  },
+];
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className={styles["head-text"]}>
+        A Public Service Leader{" "}
+        <span>with skills in Full Stack Development</span>
+      </h2>
+
+      <div className={styles.app__profiles}>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className={styles["app__profile"]}
+            key={about.title + index}
+          >
+            <img src={about.imgURL} alt={about.title + " IMG"} />
+            <h2 className={styles["bold-text"]}>hello</h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
