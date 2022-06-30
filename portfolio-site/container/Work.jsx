60a687ac6875f8e7c72edf65551586d3419563ee
Devin MacGillivray <devhmac@gmail.com>
2022-06-30 06:40:35 -0600

    building work filter

--- portfolio-site/container/Work.jsx
+++ portfolio-site/container/Work.jsx
@@ -1,7 +1,35 @@
 import styles from "../styles/Work.module.scss";
+import { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../components/AppWrap";
+import { urlFor, client } from "../client";
 
 const Work = () => {
-  return <div></div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className={styles["head-text"]}>
+        My <span>Work</span>
+      </h2>
+      <div className={styles["app__work-filter"]}>
+        {["Full Stack", "Front End", "React", "All"].map((item, index) => {
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`${styles["app__work-filter-item"]} ${
+              styles["app__flex"]
+            } ${styles["ap-text"]} ${
+              activeFilter === item ? styles["item-active"] : ""
+            }`}
+          ></div>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Work;
