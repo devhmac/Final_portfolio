64fd0c46a97018ce6166e9d4d6d9d4831816ec7b
Devin MacGillivray <devhmac@gmail.com>
2022-07-02 12:00:46 -0600

    adding social media links

--- portfolio-site/components/SocialMedia.jsx
+++ portfolio-site/components/SocialMedia.jsx
@@ -5,21 +5,26 @@ import {
   FaTwitterSquare,
 } from "react-icons/fa";
 import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
 const SocialMedia = () => {
   return (
     <div className={styles.app__social}>
       <div>
-        <FaLinkedin />
+        <Link title="To Linkedin" href="https://www.linkedin.com/in/devhmac/">
+          <FaLinkedin />
+        </Link>
+      </div>
+
+      <div>
+        <Link title="To Github" href="https://github.com/devhmac">
+          <FaGithub />
+        </Link>
       </div>
       <div>
-        <FaGithub />
-      </div>
-      <div>
-        <FaMedium />
-      </div>
-      <div>
-        <FaTwitterSquare />
+        <Link title="To Medium" href="https://medium.com/@devhmac">
+          <FaMedium />
+        </Link>
       </div>
     </div>
   );
