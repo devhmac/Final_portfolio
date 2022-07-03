ccd54143918af3796b95742cdbbd5ac92984ed23
Devin MacGillivray <devhmac@gmail.com>
2022-07-03 16:33:06 -0600

    styling skills and tech row

--- portfolio-site/components/SkillsRow.jsx
+++ portfolio-site/components/SkillsRow.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import styles from "../styles/SkillsRow.module.scss";
 
 const SkillsRow = () => {
   return (
-    <div>
-      <h2>Skills and Technology</h2>
-    </div>
+    <>
+      <div style={styles.skills_title}>
+        <h2>Skills and Technology</h2>
+        <p>Currently Studying: Data Analysis</p>
+      </div>
+      <div className={styles["skills-block"]}>
+        <div>
+          <h3>Languages</h3>
+          <ul>
+            <li>Javascript</li>
+            <li>SQL</li>
+            <li>R</li>
+            <li>Ruby</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Front End</h3>
+          <ul>
+            <li>React</li>
+            <li>jQuery</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Back End</h3>
+          <ul>
+            <li>NodeJS</li>
+            <li>ExpressJS</li>
+            <li>Ruby On Rails</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Databases</h3>
+          <ul>
+            <li>PostgreSQL</li>
+            <li>ERD Design</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Data Analysis</h3>
+          <ul>
+            <li>Data scraping & cleaning</li>
+            <li>Tableau</li>
+            <li>Google Data Studio</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
