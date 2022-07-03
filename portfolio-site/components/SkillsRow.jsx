b3b368a8618a52abede6d7f550db29611315e7ec
Devin MacGillivray <devhmac@gmail.com>
2022-07-03 16:48:42 -0600

    styling'

--- portfolio-site/components/SkillsRow.jsx
+++ portfolio-site/components/SkillsRow.jsx
@@ -3,14 +3,15 @@ import styles from "../styles/SkillsRow.module.scss";
 const SkillsRow = () => {
   return (
     <>
-      <div style={styles.skills_title}>
-        <h2>Skills and Technology</h2>
-        <p>Currently Studying: Data Analysis</p>
-      </div>
+      <h2>Skills and Technology</h2>
+      <p className={`${styles["p-text"]} ${styles["studying"]}`}>
+        Currently Studying: Google Data Analytics Professional Certificate
+      </p>
+
       <div className={styles["skills-block"]}>
         <div>
           <h3>Languages</h3>
-          <ul>
+          <ul className={styles["p-text"]}>
             <li>Javascript</li>
             <li>SQL</li>
             <li>R</li>
@@ -19,7 +20,7 @@ const SkillsRow = () => {
         </div>
         <div>
           <h3>Front End</h3>
-          <ul>
+          <ul className={styles["p-text"]}>
             <li>React</li>
             <li>jQuery</li>
             <li>HTML</li>
@@ -28,7 +29,7 @@ const SkillsRow = () => {
         </div>
         <div>
           <h3>Back End</h3>
-          <ul>
+          <ul className={styles["p-text"]}>
             <li>NodeJS</li>
             <li>ExpressJS</li>
             <li>Ruby On Rails</li>
@@ -36,14 +37,14 @@ const SkillsRow = () => {
         </div>
         <div>
           <h3>Databases</h3>
-          <ul>
+          <ul className={styles["p-text"]}>
             <li>PostgreSQL</li>
             <li>ERD Design</li>
           </ul>
         </div>
         <div>
           <h3>Data Analysis</h3>
-          <ul>
+          <ul className={styles["p-text"]}>
             <li>Data scraping & cleaning</li>
             <li>Tableau</li>
             <li>Google Data Studio</li>
