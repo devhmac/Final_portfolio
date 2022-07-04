926578354c7c05a9b7fc1a2ee081b74910e8dc51
Devin MacGillivray <devhmac@gmail.com>
2022-07-04 12:04:06 -0600

    bringing in styling

--- portfolio-site/container/Blog.jsx
+++ portfolio-site/container/Blog.jsx
@@ -3,16 +3,22 @@ import matter from "gray-matter";
 import Image from "next/image";
 import Link from "next/link";
 import BlogPost from "../components/BlogPost";
-import { AppWrap } from "../components/AppWrap";
+
+import styles from "../styles/Home.module.scss";
 
 const Blog = ({ posts }) => {
   return (
-    <div id="articles%20&%20case%20studies">
-      Blog
-      {posts.map((post, index) => {
-        return <BlogPost key={index} post={post} />;
-      })}
-    </div>
+    <>
+      <h2 className={styles["head-text"]}>
+        Articles & <span>Case Studies</span>
+      </h2>
+      <div id="articles%20&%20case%20studies">
+        Blog
+        {posts.map((post, index) => {
+          return <BlogPost key={index} post={post} />;
+        })}
+      </div>
+    </>
   );
 };
 
