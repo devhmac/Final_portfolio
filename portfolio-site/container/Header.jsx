eaac55797ff5aa883ad0ced7ab8f8ab48265fac5
Devin MacGillivray <devhmac@gmail.com>
2023-03-04 13:26:06 -0700

    update landing text

--- portfolio-site/container/Header.jsx
+++ portfolio-site/container/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
         </div>
         <div className={` ${styles["scroll-indicator-top"]}`}>
           <p>
-            More about me, and my work
+            More about me
             <span> below</span>
           </p>
           {/* <BsChevronDoubleDown /> */}
