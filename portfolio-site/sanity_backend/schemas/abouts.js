6372790d3db13a032a7fb294d772743b6c1fcf35
Devin MacGillivray <devhmac@gmail.com>
2023-11-19 22:13:46 -0700

    building in Abouts section skills ordering, and added some photos

--- portfolio-site/sanity_backend/schemas/abouts.js
+++ portfolio-site/sanity_backend/schemas/abouts.js
@@ -1,26 +1,38 @@
-export default{
-    name:'abouts',
-    title:'Abouts',
+export default {
+    name: 'abouts',
+    title: 'Abouts',
     type: 'document',
-    fields:[
+    fields: [
         {
-            name:'title',
-            title:'Title',
-            type:'string'
+            name: 'title',
+            title: 'Title',
+            type: 'string'
         },
         {
-            name:'description',
-            title:'Description',
-            type:'string'
+            name: 'description',
+            title: 'Description',
+            type: 'string'
         },
         {
-            name:'imgUrl',
-            title:'ImgUrl',
+            name: 'imgUrl',
+            title: 'ImgUrl',
             type: 'image',
             options: {
-              hotspot: true,
+                hotspot: true,
             },
         },
-        
-    ]
+        {
+            name: 'order',
+            title: 'Order Number',
+            type: 'number',
+        }
+    ],
+    orderings: [
+        {
+            title: 'Order Number',
+            name: 'order',
+            by: [
+                { field: 'order', direction: 'desc' }
+            ]
+        }]
 }
